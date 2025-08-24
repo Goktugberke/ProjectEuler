8ce7a9da861f121a68dbff9360de1f5c0c7043dd
Göktuğ Berke Güngören <132768516+Goktugberke@users.noreply.github.com>
2025-08-24 14:38:49 +0300

    AI solutions added

--- src/Problem_35.java
+++ src/Problem_35.java
@@ -2,6 +2,7 @@
 //There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
 //How many circular primes are there below one million?
 
+//My Solution
 public class Problem_35 {
     public static void main(String[] args) {
         System.out.println(countCircularPrimes(1000000));
@@ -33,3 +34,34 @@ public class Problem_35 {
         return true;
     }
 }
+
+//AI's Solution
+//public class CircularPrimes {
+//    static boolean[] isPrime = new boolean[1000001];
+//    static void sieve() {
+//        for (int i = 0; i <= 1000000; i++) isPrime[i] = true;
+//        isPrime[0] = isPrime[1] = false;
+//        for (int i = 2; i * i <= 1000000; i++)
+//            if (isPrime[i])
+//                for (int j = i * i; j <= 1000000; j += i)
+//                    isPrime[j] = false;
+//    }
+//
+//    static boolean isCircularPrime(int n) {
+//        String s = String.valueOf(n);
+//        int len = s.length();
+//        for (int i = 0; i < len; i++) {
+//            int rotated = Integer.parseInt(s.substring(i) + s.substring(0, i));
+//            if (rotated > 1000000 || !isPrime[rotated]) return false;
+//        }
+//        return true;
+//    }
+//
+//    public static void main(String[] args) {
+//        sieve();
+//        int count = 0;
+//        for (int i = 2; i < 1000000; i++)
+//            if (isPrime[i] && isCircularPrime(i)) count++;
+//        System.out.println(count);
+//    }
+//}
--- src/Problem_36.java
+++ src/Problem_36.java
@@ -2,6 +2,7 @@
 //Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 //(Please note that the palindromic number, in either base, may not include leading zeros.)
 
+//My Solution
 public class Problem_36 {
     public static void main(String[] args) {
         System.out.println(doubleBasedPalindromes(1000000));
@@ -49,3 +50,37 @@ public class Problem_36 {
         return true;
     }
 }
+
+//AI's Solution
+//public class DoubleBasePalindromes {
+//    static boolean isPalindrome(String s) {
+//        int left = 0, right = s.length() - 1;
+//        while (left < right) {
+//            if (s.charAt(left++) != s.charAt(right--)) return false;
+//        }
+//        return true;
+//    }
+//
+//    static String toBinary(int n) {
+//        if (n == 0) return "0";
+//        StringBuilder sb = new StringBuilder();
+//        int num = n;
+//        while (num > 0) {
+//            sb.append(num % 2);
+//            num /= 2;
+//        }
+//        return sb.toString();
+//    }
+//
+//    public static void main(String[] args) {
+//        long sum = 0;
+//        for (int i = 1; i < 1000000; i++) {
+//            String dec = String.valueOf(i);
+//            String bin = toBinary(i);
+//            if (isPalindrome(dec) && isPalindrome(bin)) {
+//                sum += i;
+//            }
+//        }
+//        System.out.println(sum);
+//    }
+//}
